1cbed63c3d88209d1935fc413d03ab56f92e4680
vivekssharma07 <vivekssharma07@gmail.com>
2018-05-22 15:15:35 +0530

    18.4.4 Release

---
+++ js/config.js
@@ -0,0 +1,2 @@
+
+var reg=/^([\w-\.]+@(?!gmail.com)(?!yahoo.com)(?!yahoo.co.in)(?!yahoo.in)(?!hotmail.com)(?!yahoo.co.in)(?!aol.com)(?!abc.com)(?!xyz.com)(?!pqr.com)(?!rediffmail.com)(?!live.com)(?!outlook.com)(?!me.com)(?!msn.com)(?!ymail.com)([\w-]+\.)+[\w-]{2,4})?$/;
